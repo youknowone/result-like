d036e59477afd53142d26d44adb34e75ad59beb8
Jeong YunWon <jeong@youknowone.org>
2020-09-20 05:45:20 +0900

    0.3.0 with doc

--- README.md
+++ README.md
@@ -3,13 +3,13 @@
 Install: [https://crates.io/crates/result-like](https://crates.io/crates/result-like)
 
 Define your own Option-like and Result-like enum types.
-Do not redefine option and result utilities to your own enums.
+Avoid to reimplement everything of option and result for your own enums.
 
 Option example
 ```rust
 use result_like::option_like;
 
-// Simple case with single argument name
+// Simple case with single argument name to use Some and None
 option_like!(MyOption);
 
 let v = MyOption::Some(1);
@@ -20,7 +20,7 @@ assert!(v.unwrap() == 1);
 let opt = v.into_option();
 assert!(opt == Some(1));
 
-// pub enum with custom names
+// pub enum with custom names instead of Some and None
 option_like!(pub Number, Value, Nan);
 
 let v = Number::Value(10);
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,43 @@
+//! # OptionLike and ResultLike
+//!
+//! Define your own Option-like and Result-like enum types.
+//! Avoid to reimplement everything of [std::option::Option] and [std::result::Result] for your own enums.
+//!
+//! Option example
+//! ```rust
+//! use result_like::option_like;
+//!
+//! // Simple case with single argument name to use Some and None
+//! option_like!(MyOption);
+//!
+//! let v = MyOption::Some(1);
+//! // every option utilities are possible including unwrap, map, and, or etc.
+//! assert!(v.unwrap() == 1);
+//!
+//! // convertable to option
+//! let opt = v.into_option();
+//! assert!(opt == Some(1));
+//!
+//! // pub enum with custom names instead of Some and None
+//! option_like!(pub Number, Value, Nan);
+//!
+//! let v = Number::Value(10);
+//! assert!(v != Number::Nan);
+//! ```
+//!
+//! Result example in same way
+//! ```rust
+//! use result_like::result_like;
+//!
+//! // simply,
+//! result_like!(pub(crate) MyResult);
+//!
+//! // customizing,
+//! result_like!(Trial, Success, Failure);
+//! ```
+//!
+//! Try [result_like::impl_option_like] and [result_like::impl_result_like] to only append impls to your enum.
+
 mod option;
 pub use option::*;
 
